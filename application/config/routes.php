e5c34744991f3cba26d4859b455178979dc81ce1
Aleksandr <aleks120591@gmail.com>
2017-02-21 15:19:46 +0400

    111

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,6 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+$route['admin/login'] ='admin/lunit/login';
+$route['admin/logout'] ='admin/lunit/logout';
+$route[''] ='';
+
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
